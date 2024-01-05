fa0ebbd2d65b7d694882c07f364177616579fb4a
Beellal97242599@ <mahamanehabiboubilal999@gmail.com>
2024-01-05 19:06:53 +0100

    Update

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,9 +1,80 @@
 import React from 'react';
-import './Blog.css'
+import './Blog.css';
+import Image from '../../assets/Me.jpg'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <section className="blog container section" id='blog'>
+      <h2 className="section">Les derniers posts</h2>
+
+      <div className="blog_container grid">
+
+        
+        <div className="blog_card">
+          <div className="blog_thumb">
+            <a href="#">
+              <span className="blog_category">Review</span>
+            </a>
+            <a href="#">
+                <img src={Image} alt="" className="blog_img" />
+            </a>
+          </div>
+
+          <div className="blog_details">
+            <h3 className="blog_title">5 Bonnes Application pour le developpement</h3>
+            <div className="blog_meta">
+              <span>01 Janvier 2024</span>
+              <span className="blog_dot">.</span>
+              <span>Beellal</span>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="blog_card">
+          <div className="blog_thumb">
+            <a href="#">
+              <span className="blog_category">Review</span>
+            </a>
+            <a href="#">
+                <img src={Image} alt="" className="blog_img" />
+            </a>
+          </div>
+
+          <div className="blog_details">
+            <h3 className="blog_title">5 Bonnes Application pour le developpement</h3>
+            <div className="blog_meta">
+              <span>01 Janvier 2024</span>
+              <span className="blog_dot">.</span>
+              <span>Beellal</span>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="blog_card">
+          <div className="blog_thumb">
+            <a href="#">
+              <span className="blog_category">Review</span>
+            </a>
+            <a href="#">
+                <img src={Image} alt="" className="blog_img" />
+            </a>
+          </div>
+
+          <div className="blog_details">
+            <h3 className="blog_title">5 Bonnes Application pour le developpement</h3>
+            <div className="blog_meta">
+              <span>01 Janvier 2024</span>
+              <span className="blog_dot">.</span>
+              <span>Beellal</span>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </section>
   )
 }
 
