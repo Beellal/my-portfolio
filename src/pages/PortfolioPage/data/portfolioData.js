04757de8aebb6a1a1824456ae96e08e40c813176
Beellal-00 <98529296+Beellal@users.noreply.github.com>
2024-12-02 19:14:53 +0100

    Update src/pages/PortfolioPage/data/portfolioData.js [BoltSync]

---
+++ src/pages/PortfolioPage/data/portfolioData.js
@@ -0,0 +1,27 @@
+import Work1 from '../../../assets/S1.png';
+import Work3 from '../../../assets/EFST (1).png';
+import Work4 from '../../../assets/pharma.png';
+
+export const portfolioData = [
+  {
+    id: 1,
+    image: Work1,
+    title: 'Y-Builders',
+    category: 'BTP',
+    link: '#'
+  },
+  {
+    id: 2,
+    image: Work3,
+    title: 'English For The Sahel',
+    category: 'Projets Developpement',
+    link: '#'
+  },
+  {
+    id: 3,
+    image: Work4,
+    title: 'Pharma App',
+    category: 'Developpement_Web',
+    link: '#'
+  }
+];
